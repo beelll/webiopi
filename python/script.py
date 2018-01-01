7a7c3561069f95cc6a05fe448e0c2e2dcb6fb633
BelllTree <BelllTree@192.168.0.11>
2018-01-01 16:18:13 +0900

    try

--- python/script.py
+++ python/script.py
@@ -1,45 +1,76 @@
 import webiopi
 import datetime
- 
+
 GPIO = webiopi.GPIO
- 
-LIGHT = 23 # GPIO pin using BCM numbering
- 
-HOUR_ON  = 8  # Turn Light ON at 08:00
-HOUR_OFF = 18 # Turn Light OFF at 18:00
- 
+
+#LIGHT = 23 # GPIO pin using BCM numbering
+
+
+IO_AIRCON_ON = 6
+IO_AIRCON_OFF = 13
+IO_LIGHT_PC_ROOM = 23
+IO_TV_POWER = 4
+
+#HOUR_ON  = 8  # Turn Light ON at 08:00
+#HOUR_OFF = 18 # Turn Light OFF at 18:00
+
+
 # setup function is automatically called at WebIOPi startup
 def setup():
     # set the GPIO used by the light to output
-    GPIO.setFunction(LIGHT, GPIO.OUT)
- 
+    GPIO.setFunction(IO_AIRCON_ON, GPIO.OUT)
+    GPIO.setFunction(IO_AIRCON_OFF, GPIO.OUT)
+    GPIO.setFunction(IO_LIGHT_PC_ROOM, GPIO.OUT)
+    GPIO.setFunction(IO_TV_POWER, GPIO.OUT)
+
     # retrieve current datetime
-    now = datetime.datetime.now()
- 
+    #now = datetime.datetime.now()
+
     # test if we are between ON time and tun the light ON
-    if ((now.hour >= HOUR_ON) and (now.hour < HOUR_OFF)):
-        GPIO.digitalWrite(LIGHT, GPIO.HIGH)
- 
-# loop function is repeatedly called by WebIOPi 
+    #if ((now.hour >= HOUR_ON) and (now.hour < HOUR_OFF)):
+    #    GPIO.digitalWrite(LIGHT, GPIO.HIGH)
+
+
+
+# loop function is repeatedly called by WebIOPi
 def loop():
     # retrieve current datetime
-    now = datetime.datetime.now()
- 
+    #now = datetime.datetime.now()
+
     # toggle light ON all days at the correct time
-    if ((now.hour == HOUR_ON) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT, GPIO.HIGH)
- 
+    #if ((now.hour == HOUR_ON) and (now.minute == 0) and (now.second == 0)):
+    #    if (GPIO.digitalRead(LIGHT) == GPIO.LOW):
+    #        GPIO.digitalWrite(LIGHT, GPIO.HIGH)
+
     # toggle light OFF
-    if ((now.hour == HOUR_OFF) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT) == GPIO.HIGH):
-            GPIO.digitalWrite(LIGHT, GPIO.LOW)
- 
+    #if ((now.hour == HOUR_OFF) and (now.minute == 0) and (now.second == 0)):
+    #    if (GPIO.digitalRead(LIGHT) == GPIO.HIGH):
+    #        GPIO.digitalWrite(LIGHT, GPIO.LOW)
+
     # gives CPU some time before looping again
     webiopi.sleep(1)
- 
-# destroy function is called at WebIOPi shutdown
-def destroy():
-    GPIO.digitalWrite(LIGHT, GPIO.LOW)
 
+
+
+# destroy function is called at WebIOPi shutdown
+#def destroy():
+    #GPIO.digitalWrite(LIGHT, GPIO.LOW)
+
+
+@webiopi.macro
+def setGpio(value):
+    print("Call setGpio")
+    print(value)
+
+    #if value == "23":
+    GPIO.digitalWrite(IO_LIGHT_PC_ROOM, GPIO.HIGH)
+    print("High")
+    GPIO.digitalWrite(IO_LIGHT_PC_ROOM, GPIO.LOW)
+    print("LOW")
+    #webiopi.sleep(1)
+        #subprocess.call(["sudo", "./home/pi/Desktop/Study/Servomotor/turnLeft"])
+    #else:
+    #    GPIO.digitalWrite(IO_LIGHT_PC_ROOM, GPIO.LOW)
+    #    print("Low")
+    return 1
 

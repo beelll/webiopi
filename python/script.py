303114711795d0298678a2cade38f5ed7ca80a7c
BelllTree <BelllTree@192.168.0.40>
2018-01-09 23:18:13 +0900

    try

--- python/script.py
+++ python/script.py
@@ -11,9 +11,10 @@ IO_AIRCON_OFF = 13
 IO_LIGHT_PC_ROOM = 23
 IO_TV_POWER = 4
 
-#HOUR_ON  = 8  # Turn Light ON at 08:00
-#HOUR_OFF = 18 # Turn Light OFF at 18:00
-
+HOUR_ON  = 5  # Turn ON  at 05:00
+MINUTE_ON  = 20
+HOUR_OFF = 7  # Turn OFF at 07:00
+MINUTE_OFF = 18
 
 # setup function is automatically called at WebIOPi startup
 def setup():
@@ -24,7 +25,7 @@ def setup():
     GPIO.setFunction(IO_TV_POWER, GPIO.OUT)
 
     # retrieve current datetime
-    #now = datetime.datetime.now()
+    now = datetime.datetime.now()
 
     # test if we are between ON time and tun the light ON
     #if ((now.hour >= HOUR_ON) and (now.hour < HOUR_OFF)):
@@ -35,17 +36,15 @@ def setup():
 # loop function is repeatedly called by WebIOPi
 def loop():
     # retrieve current datetime
-    #now = datetime.datetime.now()
+    now = datetime.datetime.now()
 
-    # toggle light ON all days at the correct time
-    #if ((now.hour == HOUR_ON) and (now.minute == 0) and (now.second == 0)):
-    #    if (GPIO.digitalRead(LIGHT) == GPIO.LOW):
-    #        GPIO.digitalWrite(LIGHT, GPIO.HIGH)
+    # toggle ON all days at the correct time
+    if ((now.hour == HOUR_ON) and (now.minute == 0) and (now.second == 0)):
+        setGpio(IO_AIRCON_ON)
 
-    # toggle light OFF
-    #if ((now.hour == HOUR_OFF) and (now.minute == 0) and (now.second == 0)):
-    #    if (GPIO.digitalRead(LIGHT) == GPIO.HIGH):
-    #        GPIO.digitalWrite(LIGHT, GPIO.LOW)
+    # toggle OFF
+    if ((now.hour == HOUR_OFF) and (now.minute == MINUTE_OFF) and (now.second == 0)):
+        setGpio(IO_AIRCON_OFF)
 
     # gives CPU some time before looping again
     webiopi.sleep(1)

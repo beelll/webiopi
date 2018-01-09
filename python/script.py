ee5bd1c2e8d8a5417ff7f28217e7941c8b8c8d11
BelllTree <BelllTree@192.168.0.40>
2018-01-09 23:48:58 +0900

    try

--- python/script.py
+++ python/script.py
@@ -14,7 +14,7 @@ IO_TV_POWER = 4
 HOUR_ON  = 5  # Turn ON  at 05:00
 MINUTE_ON  = 30
 HOUR_OFF = 23  # Turn OFF at 07:00
-MINUTE_OFF = 44
+MINUTE_OFF = 50
 
 # setup function is automatically called at WebIOPi startup
 def setup():
@@ -41,6 +41,7 @@ def loop():
     # toggle ON all days at the correct time
     if ((now.hour == HOUR_ON) and (now.minute == MINUTE_ON) and (now.second == 0)):
         #setGpio(IO_AIRCON_ON)
+        print("Call ON 1")
         GPIO.digitalWrite(IO_AIRCON_ON, GPIO.LOW)
         webiopi.sleep(0.5)
         GPIO.digitalWrite(IO_AIRCON_ON, GPIO.HIGH)
@@ -48,6 +49,7 @@ def loop():
     # toggle OFF
     if ((now.hour == HOUR_OFF) and (now.minute == MINUTE_OFF) and (now.second == 0)):
         #setGpio(IO_AIRCON_OFF)
+        print("Call OFF 1")
         GPIO.digitalWrite(IO_AIRCON_OFF, GPIO.LOW)
         webiopi.sleep(0.5)
         GPIO.digitalWrite(IO_AIRCON_OFF, GPIO.HIGH)

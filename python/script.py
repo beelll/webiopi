4dcd7863795a81e9b6832d999077a179da213d21
BelllTree <BelllTree@192.168.0.40>
2018-01-14 16:30:35 +0900

    try

--- python/script.py
+++ python/script.py
@@ -20,8 +20,7 @@ HOUR_OFF = 22  # Turn OFF at 07:00
 MINUTE_OFF = 49
 # setup function is automatically called at WebIOPi startup
 def setup():
-
-    webiopi.sleep(10)
+    webiopi.sleep(5)
 
     # set the GPIO used by the light to output
     GPIO.setFunction(IO_AIRCON_ON, GPIO.OUT)
@@ -42,8 +41,7 @@ def setup():
 def loop():
     # retrieve current datetime
     now = datetime.datetime.now()
-    print("Call loop!")
-    webiopi.debug(">> Call loop")
+    #webiopi.debug(">> Call loop")
 
     # toggle ON all days at the correct time
     if ((now.hour == HOUR_ON) and (now.minute == MINUTE_ON) and (now.second == 0)):
@@ -51,17 +49,13 @@ def loop():
         GPIO.digitalWrite(IO_AIRCON_ON, GPIO.LOW)
         webiopi.sleep(0.5)
         GPIO.digitalWrite(IO_AIRCON_ON, GPIO.HIGH)
-        #print("Call ON 1")
 
     # toggle OFF
     if ((now.hour == HOUR_OFF) and (now.minute == MINUTE_OFF) and (now.second == 0)):
         #setGpio(IO_AIRCON_OFF)
-        #webiopi.debug("Call OFF 1")
-        #print("Call OFF 1")
         GPIO.digitalWrite(IO_AIRCON_OFF, GPIO.LOW)
         webiopi.sleep(0.5)
         GPIO.digitalWrite(IO_AIRCON_OFF, GPIO.HIGH)
-        #webiopi.debug("Call OFF 2")
 
     if (GPIO.digitalRead(IO_LIGHT) == GPIO.LOW):
         GPIO.digitalWrite(IO_LIGHT, GPIO.HIGH)
@@ -85,11 +79,9 @@ def loop():
 @webiopi.macro
 def setGpio(ioNum):
     gpio = int(ioNum)
-    #print(gpio)
     GPIO.digitalWrite(gpio, GPIO.LOW)
     webiopi.sleep(0.5)
     GPIO.digitalWrite(gpio, GPIO.HIGH)
-    #print("High")
     #print("Low")
     #webiopi.sleep(1)
         #subprocess.call(["sudo", "./home/pi/Desktop/Study/Servomotor/turnLeft"])

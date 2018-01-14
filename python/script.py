28ab5d760516343a268abfcfaf5a91ca79a1f83f
BelllTree <BelllTree@192.168.0.40>
2018-01-14 17:59:00 +0900

    try

--- python/script.py
+++ python/script.py
@@ -11,10 +11,10 @@ IO_AIRCON_OFF = 13
 IO_LIGHT_PC_ROOM = 23
 IO_TV_POWER = 4
 
-HOUR_ON  = 17  # Turn ON  at 05:00
-MINUTE_ON  = 49
-HOUR_OFF = 17  # Turn OFF at 07:00
-MINUTE_OFF = 48
+HOUR_ON  = 18  # Turn ON  at 05:00
+MINUTE_ON  = 2
+HOUR_OFF = 18  # Turn OFF at 07:00
+MINUTE_OFF = 1
 
 DATE_MONDAY = 0
 DATE_TUESDAY = 1
@@ -35,14 +35,6 @@ def setup():
     GPIO.setFunction(IO_LIGHT_PC_ROOM, GPIO.OUT)
     GPIO.setFunction(IO_TV_POWER, GPIO.OUT)
 
-    # retrieve current datetime
-    now = datetime.datetime.now()
-
-    # test if we are between ON time and tun the light ON
-    #if ((now.hour >= HOUR_ON) and (now.hour < HOUR_OFF)):
-    #    GPIO.digitalWrite(LIGHT, GPIO.HIGH)
-
-
 
 # loop function is repeatedly called by WebIOPi
 def loop():
@@ -50,11 +42,11 @@ def loop():
 
     # retrieve current datetime
     now = datetime.datetime.now()
+    today = datetime.date()
 
     # Exceptionally, don't execute program at holiday
-    #if ((now.weekday() == DATE_SATURDAY) or (now.weekday() == DATE_SUNDAY)):
-    #if ((now.weekday() == DATE_SATURDAY)):
-     #   return
+    if ((today.weekday() == DATE_SATURDAY)):
+        return
 
     # toggle ON all days at the correct time
     if ((now.hour == HOUR_ON) and (now.minute == MINUTE_ON) and (now.second == 0)):

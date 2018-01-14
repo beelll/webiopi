85534befaf1c2ad79f3f9b27955489c9fb4f2315
BelllTree <BelllTree@192.168.0.40>
2018-01-14 18:15:35 +0900

    try

--- python/script.py
+++ python/script.py
@@ -1,5 +1,6 @@
 import webiopi
 import datetime
+#import date
 
 webiopi.setDebug()
 
@@ -12,9 +13,9 @@ IO_LIGHT_PC_ROOM = 23
 IO_TV_POWER = 4
 
 HOUR_ON  = 18  # Turn ON  at 05:00
-MINUTE_ON  = 7
+MINUTE_ON  = 19
 HOUR_OFF = 18  # Turn OFF at 07:00
-MINUTE_OFF = 6
+MINUTE_OFF = 18
 
 DATE_MONDAY = 0
 DATE_TUESDAY = 1
@@ -41,12 +42,13 @@ def loop():
     webiopi.debug(">> Call loop")
 
     # retrieve current datetime
-    now = datetime.datetime.now()
-    today = datetime.date()
+    #now = datetime.datetime.now()
+    now = datetime.datetime.today()
+    #today = date.date()
 
     # Exceptionally, don't execute program at holiday
-    #if ((today.weekday() == DATE_SATURDAY)):
-    #    return
+    if ((now.weekday() == DATE_SATURDAY)):
+        return
 
     # toggle ON all days at the correct time
     if ((now.hour == HOUR_ON) and (now.minute == MINUTE_ON) and (now.second == 0)):

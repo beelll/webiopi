80aecfb073dd6a61077a417a10877930bfdd66da
BelllTree <BelllTree@192.168.0.40>
2018-07-03 22:57:37 +0900

    ir

--- python/script.py
+++ python/script.py
@@ -124,6 +124,6 @@ def setGpio(ioNum):
 @webiopi.macro
 def sendIr(dummy):
     webiopi.debug(">> Call sendIr")
-    #subprocess.call(["python", "IR-remocon02-commandline.py", "t", "'cat data_dir/airconPowerOff.dat'"], cwd = '../I2C0x52-IR')
+    subprocess.call(["python", "IR-remocon02-commandline.py", "t", "'cat data_dir/airconPowerOff.dat'"], cwd = '../I2C0x52-IR')
     return 1
 

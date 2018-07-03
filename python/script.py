d0cf2f6f8cda697aff954fdaaaf43f87d196ee55
BelllTree <BelllTree@192.168.0.40>
2018-07-03 23:19:36 +0900

    ir

--- python/script.py
+++ python/script.py
@@ -127,6 +127,6 @@ def sendIr(dummy):
     webiopi.debug(">> Call sendIr")
     cmd = 'python ../I2C0x52-IR/IR-remocon02-commandline.py t \`cat ../I2C0x52-IR/data_dir/airconPowerOff.dat\`'
     #subprocess.call(cmd.split())
-    subprocess.call(["python", "../I2C0x52-IR/IR-remocon02-commandline.py", "t", "'cat ../I2C0x52-IR/data_dir/airconPowerOff.dat'"])
+    subprocess.call(["python", "../I2C0x52-IR/IR-remocon02-commandline.py", "t", "\'cat ../I2C0x52-IR/data_dir/airconPowerOff.dat\'"])
     return 1
 

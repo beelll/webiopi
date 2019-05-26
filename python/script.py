1b7004cdee2f561523f6e3f9af2594f3494f094c
BelllTree <BelllTree@192.168.0.40>
2019-05-26 13:10:38 +0900

    try

--- python/script.py
+++ python/script.py
@@ -178,8 +178,10 @@ def setAirconTimer(on, off, sw):
 
 @webiopi.macro
 def getCurrentTemperature():
-    webiopi.debug(temperature.getTemperature())
-    webiopi.debug(temperature.getHumidity())
+    ret = getTempBySocket()
+    webiopi.debug('RET  : ' + ret)
+    webiopi.debug('TEMP : ' + temperature.getTemperature())
+    webiopi.debug('HUMI : ' + temperature.getHumidity())
     return "%s;%s" % (temperature.getTemperature(), temperature.getHumidity())
 
 
